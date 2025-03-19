da4793071d1a52c93f911c24857e4c378c1867e6
Akshay <akshay.rp@edgesoftinc.com>
2025-03-19 12:18:20 -0700

    commit taskagent

--- JavaSource/csapi/impl/tasks/TasksAgent.java
+++ JavaSource/csapi/impl/tasks/TasksAgent.java
@@ -10,6 +10,7 @@ public class TasksAgent {
 	
 	public static boolean emailExpiredPermits(int days, String template) {
 		Sage db = new Sage();
+		Sage db1 = new Sage();
 		boolean result = false;
 		try {
 			String content = null;
@@ -20,10 +21,11 @@ public class TasksAgent {
 				subject = db.getString("NAME");
 				content = db.getString("TEMPLATE");
 			
-				db.query(" SELECT A.ID, STUFF(( select ';' + EMAIL from REF_ACT_USERS AU JOIN REF_USERS RU ON AU.REF_USERS_ID = RU.ID AND RU.ACTIVE = 'Y' JOIN USERS U ON RU.USERS_ID = U.ID AND U.ACTIVE = 'Y' where A.ID = AU.ACTIVITY_ID AND AU.ACTIVE = 'Y' AND NOTIFY = 'Y' for xml path('')),1,1,'') EMAIL FROM ACTIVITY A WHERE exp_date = CONVERT(date, getDate() + "+ days +", 101) ");
-				while(db.next()) {
-					int actid = db.getInt("ID");
-					email = db.getString("EMAIL");
+				
+				db1.query(" SELECT A.ID, STUFF(( select ';' + EMAIL from REF_ACT_USERS AU JOIN REF_USERS RU ON AU.REF_USERS_ID = RU.ID AND RU.ACTIVE = 'Y' JOIN USERS U ON RU.USERS_ID = U.ID AND U.ACTIVE = 'Y' where A.ID = AU.ACTIVITY_ID AND AU.ACTIVE = 'Y' AND NOTIFY = 'Y' for xml path('')),1,1,'') EMAIL FROM ACTIVITY A WHERE exp_date = CONVERT(date, getDate() + "+ days +", 101) ");
+				while(db1.next()) {
+					int actid = db1.getInt("ID");
+					email = db1.getString("EMAIL");
 					if(Operator.hasValue(email)) {
 						CommunicationsAgent.email("activity", 0, email, 890, subject, content, 0, "");
 						result = true;
@@ -32,6 +34,7 @@ public class TasksAgent {
 			}
 		} finally {
 			db.clear();
+			db1.clear();
 		}
 		return result;
 	}
@@ -39,6 +42,7 @@ public class TasksAgent {
 	public static boolean updateActivity(String template) {
 		Sage db = new Sage();
 		Sage db1 = new Sage();
+		Sage db2 = new Sage();
 		String command;
 		boolean result = false;
 		int status = 0;
@@ -47,7 +51,8 @@ public class TasksAgent {
 			if(db.next()) {
 				status = db.getInt("ID");
 			}
-			
+			db.clear();
+			db = new Sage();
 			db.query("select a.id, exp_date from activity a join lkup_act_status s on a.lkup_act_status_id = s.id where (issued !='Y' and expired !='Y') and exp_date <= getdate() and exp_date >= '2025-01-31' ");
 			while(db.next()) {
 				int actid = db.getInt("ID");
@@ -58,6 +63,7 @@ public class TasksAgent {
 				if (db1.update(command)) {
 					result = true;
 				}
+				db1.clear();
 				
 				if(result){
 					ActivityAgent.addHistory(actid, "activity", actid, "update");
@@ -65,14 +71,15 @@ public class TasksAgent {
 					String content = null;
 					String subject = null;
 					String email;
-					db.query("select * from TEMPLATE where NAME = '"+ template +"' ");
-					if(db.next()) {
-						subject = db.getString("NAME");
-						content = db.getString("TEMPLATE");
+					db2.query("select * from TEMPLATE where NAME = '"+ template +"' ");
+					if(db2.next()) {
+						subject = db2.getString("NAME");
+						content = db2.getString("TEMPLATE");
 					
-						db.query(" SELECT A.ID, STUFF(( select ';' + EMAIL from REF_ACT_USERS AU JOIN REF_USERS RU ON AU.REF_USERS_ID = RU.ID AND RU.ACTIVE = 'Y' JOIN USERS U ON RU.USERS_ID = U.ID AND U.ACTIVE = 'Y' where A.ID = AU.ACTIVITY_ID AND AU.ACTIVE = 'Y' AND NOTIFY = 'Y' for xml path('')),1,1,'') EMAIL FROM ACTIVITY A WHERE A.ID = "+ actid);
-						while(db.next()) {
-							email = db.getString("EMAIL");
+						db1 = new Sage();
+						db1.query(" SELECT A.ID, STUFF(( select ';' + EMAIL from REF_ACT_USERS AU JOIN REF_USERS RU ON AU.REF_USERS_ID = RU.ID AND RU.ACTIVE = 'Y' JOIN USERS U ON RU.USERS_ID = U.ID AND U.ACTIVE = 'Y' where A.ID = AU.ACTIVITY_ID AND AU.ACTIVE = 'Y' AND NOTIFY = 'Y' for xml path('')),1,1,'') EMAIL FROM ACTIVITY A WHERE A.ID = "+ actid);
+						while(db1.next()) {
+							email = db1.getString("EMAIL");
 							if(Operator.hasValue(email)) {
 								CommunicationsAgent.email("activity", 0, email, 890, subject, content, 0, "");
 								result = true;
@@ -85,6 +92,7 @@ public class TasksAgent {
 		} finally {
 			db.clear();
 			db1.clear();
+			db2.clear();
 		}
 		return result;
 	}
